7b31bdaf8568e8a7b9713e17719d4fec969e8077
vitoiitmBSc <21f2000070@ds.study.iitm.ac.in>
2025-02-02 16:34:14 +0530

    first commit

---
+++ marks-api-main/api/index.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+import json
+
+app = FastAPI()
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
+# Load marks data
+with open("q-vercel-python.json", "r") as file:  
+    marks_list = json.load(file)
+    # print(marks_list)
+
+# Convert list to a dictionary for fast lookup
+marks_data = {entry["name"]: entry["marks"] for entry in marks_list}
+
+@app.get("/api")
+def get_marks(name: list[str] = Query([])):
+    result = [marks_data.get(n, None) for n in name]
+    return {"marks": result}
+
+'''
+git init
+git add .
+git commit -m "Initial commit"
+Create the repository in the Github website and then connect remotely
+git remote add origin https://github.com/vitoiitmBSc/Just_check_again
+# This will push the file in to the repository
+git push -u origin master
+
+'''
---
+++ marks-api-main/requirements.txt
@@ -0,0 +1,4 @@
+fastapi==0.115.8
+pandas==2.2.3
+pyjson==1.4.1
+uvicorn==0.34.0
